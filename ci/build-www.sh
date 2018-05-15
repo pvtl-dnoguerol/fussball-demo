575ae90b081300178c9da8dc4d50aefa9020aaab
Dan Noguerol <dnoguerol@pivotal.io>
2018-05-15 18:01:54 +0200

    Added command to dynamically create production environment file.

--- ci/build-www.sh
+++ ci/build-www.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd fussball-demo/www
+echo "export const environment = { production: true, apiPrefix: 'http://fussball-service.cfapps.io', mapsApiKey: $GOOGLE_MAPS_API_KEY };" > src/environments/environment.prod.ts
 npm install
 ng build --prod
 cp -rf dist/www/* ../../build-output2/
