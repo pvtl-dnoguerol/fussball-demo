bfe40ad56bedb9828a48a175d9f635e9ca6e72e0
Dan Noguerol <dnoguerol@pivotal.io>
2018-06-11 14:22:54 +0200

    Added footer and language selector.

--- www/src/app/app.component.ts
+++ www/src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  locale = 'de';
+
+  constructor() {
+    const path: string = location.pathname.toString();
+    if (path.startsWith('/en/')) {
+      this.locale = 'en';
+    } else if (path.startsWith('/nl/')) {
+      this.locale = 'nl';
+    }
+  }
+
+  onLocaleChange(value) {
+    window.location.href = '/' + value + '/';
+  }
 }
