e032daf6a0a7ca3480b99e20d5d698c13c644754
Dan Noguerol <dnoguerol@pivotal.io>
2018-06-12 10:46:13 +0200

    Made pipeline a bit more efficient.

--- ci/prod-service.sh
+++ ci/prod-service.sh
@@ -21,20 +21,3 @@ if [ $? == 0 ]; then
 else
 	echo "No blue service route found; skipping"
 fi
-
-echo "Checking for blue www deployment"
-cf check-route ekfg-blue $CF_DOMAIN | grep 'does exist' &> /dev/null
-if [ $? == 0 ]; then
-	echo "Adding blue route to www"
-	cf map-route ekfg-blue $CF_DOMAIN -n ekfg
-	echo "Removing green www route"
-	cf unmap-route ekfg $CF_DOMAIN -n ekfg
-	echo "Deleting green www"
-	cf delete ekfg -f
-	echo "Renaming blue www to green"
-	cf rename ekfg-blue ekfg
-	echo "Removing blue www route"
-	cf delete-route $CF_DOMAIN -n ekfg-blue -f
-else
-	echo "No blue www route found; skipping"
-fi
