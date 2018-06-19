8e1721984d537b2ea0db4d93a76f3b6c12bfd368
Dan Noguerol <dnoguerol@pivotal.io>
2018-06-19 14:14:29 +0200

    Temporarily disabled non-Dutch builds.

--- ci/build-www.sh
+++ ci/build-www.sh
@@ -2,11 +2,11 @@
 cd fussball-demo-www/www
 echo "export const environment = { production: true, apiPrefix: 'http://fussball-service.cfapps.io', mapsApiKey: '$GOOGLE_MAPS_API_KEY' };" > src/environments/environment.prod.ts
 npm install
-ng build --configuration=production
-ng build --configuration=production-en
+#ng build --configuration=production
+#ng build --configuration=production-en
 ng build --configuration=production-nl
-cp -rf dist/www/* ../../build-output2/
-mkdir ../../build-output2/en
-cp -rf dist/www-en/* ../../build-output2/en/
+#cp -rf dist/www/* ../../build-output2/
+#mkdir ../../build-output2/en
+#cp -rf dist/www-en/* ../../build-output2/en/
 mkdir ../../build-output2/nl
 cp -rf dist/www-nl/* ../../build-output2/nl/
