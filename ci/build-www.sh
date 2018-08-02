20fd227796c73c740ef9a924e066519688dc8963
Dan Noguerol <dnoguerol@pivotal.io>
2018-08-02 09:21:05 +0200

    Removed build optimization added for Dutch demo.

--- ci/build-www.sh
+++ ci/build-www.sh
@@ -3,10 +3,10 @@ cd fussball-demo-www/www
 echo "export const environment = { production: true, apiPrefix: 'http://fussball-service.cfapps.io', mapsApiKey: '$GOOGLE_MAPS_API_KEY' };" > src/environments/environment.prod.ts
 npm install
 ng build --source-map=false --configuration=production
-#ng build --configuration=production-en
-#ng build --configuration=production-nl
+ng build --configuration=production-en
+ng build --configuration=production-nl
 cp -rf dist/www/* ../../build-output2/
-#mkdir ../../build-output2/en
-#cp -rf dist/www-en/* ../../build-output2/en/
-#mkdir ../../build-output2/nl
-#cp -rf dist/www-nl/* ../../build-output2/nl/
+mkdir ../../build-output2/en
+cp -rf dist/www-en/* ../../build-output2/en/
+mkdir ../../build-output2/nl
+cp -rf dist/www-nl/* ../../build-output2/nl/
